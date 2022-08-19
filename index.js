f6141f0ebcc2d3087da263598aa58eed3ec3e049
martsDev <martssilvapro@gmail.com>
2022-08-19 14:56:40 -0300

    basic setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Connected on port ${port}!`));
