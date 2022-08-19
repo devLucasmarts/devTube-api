62547cf28b5133e95b3c9508cfeb2272dca68970
martsDev <martssilvapro@gmail.com>
2022-08-19 15:13:18 -0300

    setup DB connection]

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
+
+const connection = () => {
+    mongoose.connect(
+        process.env.DB_CONNECTION
+    ).then(() => {
+        console.log("Connected to DB.");
+    }).catch((err) => {
+        throw err;
+    });
+};
+
 const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Connected on port ${port}!`));
+app.listen(port, () => {
+    connection();
+    console.log(`Connected on port ${port}!`);
+});
