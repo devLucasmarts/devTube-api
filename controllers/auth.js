a8ae20e38e8251457fa438368c155f0e046774ea
martsDev <martssilvapro@gmail.com>
2022-08-19 17:12:57 -0300

    implements auth route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+import { StatusCodes } from "http-status-codes";
 
 export const signup = async (req, res) => {
 
-    console.log(req.body)
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-    // const newUser = new User(req.body);
-}
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+
+    res.status(StatusCodes.CREATED).send("User has been crated!");
+};
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import commentRoutes from './routes/comments.js';
 
 import 'express-async-errors';
 
+import { StatusCodes } from 'http-status-codes';
+
 const app = express();
 
 dotenv.config();
@@ -30,6 +32,28 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
+
+app.use((err, req, res, next) => {
+    const { name, message, details } = err;
+    console.log(`name: ${name}`);
+
+    switch (name) {
+        case 'ValidationError':
+            res.status(StatusCodes.BAD_REQUEST).json({ message: details[0].message });
+            break;
+        case 'NotFoundError':
+            res.status(StatusCodes.NOT_FOUND).json({ message });
+            break;
+        case 'ConflictError':
+            res.status(StatusCodes.CONFLICT).json({ message });
+            break;
+        default:
+            console.error(err);
+            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    };
+
+    next();
+})
 
 app.listen(port, () => {
     connection();
