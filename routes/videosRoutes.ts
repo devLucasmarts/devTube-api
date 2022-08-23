9868baa6ae443947f598adaa2362f9704758be37
martsDev <martssilvapro@gmail.com>
2022-08-23 15:37:17 -0300

    add more routes

--- routes/videosRoutes.ts
+++ routes/videosRoutes.ts
@@ -7,7 +7,11 @@ const router = express.Router();
 
 router.post("/", verifyToken, addVideo);
 router.put("/:id", verifyToken, updateVideo);
+router.put("/view/:id", verifyToken, updateVideo);
 router.delete("/:id", verifyToken, deleteVideo);
 router.get("/find/:id", getVideo);
+router.get("/trend", getVideo);
+router.get("/random", getVideo);
+router.get("/sub", getVideo);
 
 export default router;
