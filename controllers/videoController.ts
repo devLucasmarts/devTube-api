cbcdfbe598ab4fb471e8d3ec9cc958b948f0cef5
martsDev <martssilvapro@gmail.com>
2022-08-23 14:23:19 -0300

    add basic CRUD  controllers

--- controllers/videoController.ts
+++ controllers/videoController.ts
@@ -1,3 +1,18 @@
-// export const addVideo = async (req, res) => {
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
-// }
+export const addVideo = async (req: Request, res: Response) => {
+
+};
+
+export const updateVideo = async (req: Request, res: Response) => {
+
+};
+
+export const deleteVideo = async (req: Request, res: Response) => {
+
+};
+
+export const getVideo = async (req: Request, res: Response) => {
+
+};
