a56e78013491444e7e23ebea11947cf3bbc80b10
martsDev <martssilvapro@gmail.com>
2022-08-23 14:24:49 -0300

    add basic CRUD routes

--- routes/videosRoutes.ts
+++ routes/videosRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+
+import { addVideo, deleteVideo, getVideo, updateVideo } from '../controllers/videoController';
 import { verifyToken } from '../middlewares/userMiddlewares';
 
 const router = express.Router();
 
-router.post("/", verifyToken, )
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", verifyToken, getVideo);
 
 export default router;
