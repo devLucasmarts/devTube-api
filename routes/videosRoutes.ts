6de6576f9115e16d8c80b386756fa5cfb01a265f
martsDev <martssilvapro@gmail.com>
2022-08-23 11:33:31 -0300

    add basic route to add video

--- routes/videosRoutes.ts
+++ routes/videosRoutes.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { verifyToken } from '../middlewares/userMiddlewares';
 
 const router = express.Router();
+
+router.post("/", verifyToken, )
 
 export default router;
