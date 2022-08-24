a0d09552b2839718cc5bf40b404f7a13ba873e02
martsDev <martssilvapro@gmail.com>
2022-08-24 14:08:18 -0300

    implements add comment function

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { createCommentServices } from "../services/commentsServices";
 
-export const addComment = async (req: Request, res: Response) => {
+export const addComment = async (req: any, res: Response) => {
 
+    const { videoId, userComment } = req.body;
+    const { id } = req.user;
+
+    const newComment = await createCommentServices(id, videoId, userComment);
+
+    return res.status(StatusCodes.CREATED).json(newComment);
 };
 
 export const deleteComment = async (req: Request, res: Response) => {
--- services/commentsServices.ts
+++ services/commentsServices.ts
@@ -1,7 +1,11 @@
 import Comment from "../models/Comment";
 
-export const createCommentServices = async () => {
+export const createCommentServices = async (id: string, videoId: string, userComment: string) => {
+    const newComment = new Comment({  userId: id, videoId, userComment  });
 
+    const savedComment = await newComment.save();
+
+    return savedComment;
 };
 
 export const deleteCommentServices = async () => {
